de8e75620c7e2f238a78bf30fb645b6837f26d02
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-12-16 12:50:42 +0800

    chore: add Clarity
    Clarity captures how people use the site.

--- public/index.js
+++ public/index.js
@@ -1,3 +1,16 @@
+(function (c, l, a, r, i, t, y) {
+	c[a] =
+		c[a] ||
+		function () {
+			(c[a].q = c[a].q || []).push(arguments);
+		};
+	t = l.createElement(r);
+	t.async = 1;
+	t.src = 'https://www.clarity.ms/tag/' + i;
+	y = l.getElementsByTagName(r)[0];
+	y.parentNode.insertBefore(t, y);
+})(window, document, 'clarity', 'script', 'peph7az7f9');
+
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
 		navigator.serviceWorker
