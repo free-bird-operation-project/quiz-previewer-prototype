994c902e37055dec21aec1d184a9762dc2ceb2bc
Kyle Angelo Galendez <chessurisme@gmail.com>
2024-12-26 14:12:20 +0800

    chore: integrate Clarity and service worker registration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { App } from './app';
+import { loadClarity } from './script/clarity';
+import { isSW } from './script/service-worker';
 import { createLucideIcons } from './create-lucide-icons';
 
 window.addEventListener('DOMContentLoaded', async () => {
+	loadClarity('peph7az7f9');
+	isSW();
+
 	const main = document.querySelector('main');
 	main.innerHTML = '';
 	const app = await new App().render();
---
+++ src/script/service-worker.js
@@ -0,0 +1,16 @@
+function isSW() {
+	if ('serviceWorker' in navigator) {
+		window.addEventListener('load', () => {
+			navigator.serviceWorker
+				.register('./service-worker.js')
+				.then((registration) => {
+					console.log('Service Worker registered successfully', registration);
+				})
+				.catch((error) => {
+					console.error('Service Worker registration failed:', error);
+				});
+		});
+	}
+}
+
+export { isSW };
